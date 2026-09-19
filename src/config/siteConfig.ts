0e8a6a10e68f6c238f2f3c0214209024230f12f1
R-182-en <ren738535043@gmail.com>
2026-09-19 20:38:21 +0800

    这是我的博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -42,17 +42,17 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "余晖下的死鱼",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "yhxdsy",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://11408.xyz",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"这是我我的第一个博客，我会在这里记录我的学习和生活。",
 
 	// 站点关键词
 	keywords: [
@@ -117,7 +117,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly Blog",
+		title: "余晖下的死鱼",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
